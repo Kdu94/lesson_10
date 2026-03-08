35dc2322470a577eade0a7f7c9f0bd97d68c421a
Kdu94 <kdu@o2.pl>
2026-03-08 18:36:36 +0100

    add hela

---
+++ hela.py
@@ -0,0 +1,17 @@
+def hello_world():
+    print("Hello world")
+
+def sum_number(a,b):
+    return a + b
+
+def multiply_numbers(a,b):
+    return a * b
+
+def multiply_numbers(a,b):
+    return a * b
+
+def multiply_numbers(a,b):
+    return a * b
+
+def hello_world():
+    print("Hello world")
